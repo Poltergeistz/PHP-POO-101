f9bcf45229a46e6f74670db6a582e9fb78e87a8d
david <david.journo.31@gmail.com>
2018-08-20 11:53:03 +0200

    index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>POO 101</title>
+</head>
+<body>
+<?php
+
+include 'StrUtils.php';
+
+$myStr = new StrUtils('php is awesome !');
+
+echo $myStr->bold().'<br>';
+echo $myStr->italic().'<br>';
+echo $myStr->underline();
+echo $myStr->capitalize().'<br>';
+echo $myStr->uglify().'<br>';
+?>
+</body>
+</html>
